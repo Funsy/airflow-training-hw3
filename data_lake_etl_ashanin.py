225ad8a3e4323a36a1ff6c7d6eee2317fae70c79
Aleksandr Shanin <44789630+Funsy@users.noreply.github.com>
2021-04-13 12:21:53 +0300

    Update data_lake_etl_ashanin.py

--- data_lake_etl_ashanin.py
+++ data_lake_etl_ashanin.py
@@ -22,8 +22,9 @@ ods_billing = DataProcHiveOperator(
     task_id='ods_billing',
     dag=dag,
     query="""
-        insert overwrite table ashanin.ods_billing partition (year='{{ execution_date.year }}') 
-        select * from ashanin.stg_billing where year(created_at) = {{ execution_date.year }};
+        insert overwrite table ashanin.ods_billing partition (year='{{ execution_date.year }}')
+        select user_id, billing_period, service, tariff, cast(sum as DECIMAL(10,2)), cast(created_at as TIMESTAMP)
+        from ashanin.stg_billing where year(created_at) = {{ execution_date.year }};
     """,
     cluster_name='cluster-dataproc',
     job_name=USERNAME + '_ods_billing_{{ execution_date.year }}_{{ params.job_suffix }}',
@@ -69,3 +70,18 @@ ods_traffic = DataProcHiveOperator(
     params={"job_suffix": randint(0, 100000)},
     region='europe-west3',
 )
+
+ods_bytes_received_per_user = DataProcHiveOperator(
+    task_id='ods_bytes_received_per_user',
+    dag=dag,
+    query="""
+        insert overwrite table ashanin.ods_billing partition (year='{{ execution_date.year }}') 
+        select * from ashanin.stg_billing where year(created_at) = {{ execution_date.year }};
+    """,
+    cluster_name='cluster-dataproc',
+    job_name=USERNAME + '_ods_bytes_received_per_user_{{ execution_date.year }}_{{ params.job_suffix }}',
+    params={"job_suffix": randint(0, 100000)},
+    region='europe-west3',
+)
+
+ods_billing, ods_issue, ods_payment, ods_traffic >> ods_bytes_received_per_user
